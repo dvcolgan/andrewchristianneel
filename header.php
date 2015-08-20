1aae3680ad34124c651625c82a8b1f705af53eb1
David Colgan <dvcolgan@gmail.com>
2015-08-20 11:48:19 -0400

    remove all the things

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 <meta charset="UTF-8">
 <title> Andrew Christian Neel - Writer, Filmmaker and Friendly Person</title>
 
-<link rel="stylesheet" href="http://davidscolgan.webfactional.com/wp-content/themes/space-theme/style.css" media="screen" title="screen">
+<link rel="stylesheet" href="/style.css" media="screen" title="screen">
 
 </head>
 <body>
@@ -13,8 +13,6 @@
     <a href="/" id="logo-text">Andrew Christian Neel</a>
     <nav class="top-nav">
         <ul id="menu-main" class="menu">
-            <li id="menu-item-9" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-9"><a href="/">Home</a></li>
-            <li id="menu-item-11" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-11"><a href="/portfolio/">Portfolio</a></li>
         </ul>
     </nav>         
 </header>
