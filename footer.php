810fac8e691e5d897e32d8f512e713cf58c25bef
David Colgan <dvcolgan@gmail.com>
2015-08-20 11:57:03 -0400

    remove more

--- footer.php
+++ footer.php
@@ -1,7 +1,6 @@
+<hr>
 <footer id="bottom">
-    <details>
-        &copy; 2015 Andrew Christian Neel &middot; Site by <a href="http://lessboring.com">Less Boring Dev</a>
-    </details>
+    &copy; 2015 Andrew Christian Neel &middot; Site by <a href="http://lessboring.com">Less Boring Dev</a>
 </footer>
 
 </body>
--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <?php require('header.php'); ?>
 
 <section id="page">
-    <h2></h2>
     <article>
         <p>I'm A.C. Neel: writer, filmmaker, and friendly person.</p>
 
         <p>I wrote and produced the feature film We Grew Up Here (link to: http://wegrewupheremovie.com/) with 15 in the Dark in Chicago: </p>
 
-        <iframe src="https://player.vimeo.com/video/90318705" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+        <p><iframe src="https://player.vimeo.com/video/90318705" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></p>
 
         <p>As I make new things, I'll add them here.</p>
 
